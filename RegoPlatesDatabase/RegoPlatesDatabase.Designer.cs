8a6b3b92300d096059284fa7dfe50ae23b3c888a
SanyaS16 <30092329@tafe.wa.edu.au>
2024-11-18 16:23:54 +0800

    updated still needs fixes

---
+++ RegoPlatesDatabase/RegoPlatesDatabase.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace RegoPlatesDatabase
+{
+    partial class RegoPlatesDatabase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.FlaggedListBox = new System.Windows.Forms.ListBox();
+            this.saveFile = new System.Windows.Forms.SaveFileDialog();
+            this.btn_LoadData = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.SearchBar = new System.Windows.Forms.TextBox();
+            this.btnLinearSearch = new System.Windows.Forms.Button();
+            this.btnBinarySearch = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBRegoPlate = new System.Windows.Forms.TextBox();
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnTag = new System.Windows.Forms.Button();
+            this.RegoPlatesList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Tag = "";
+            this.openFileDialog1.Title = "Open File";
+            // 
+            // FlaggedListBox
+            // 
+            this.FlaggedListBox.FormattingEnabled = true;
+            this.FlaggedListBox.Location = new System.Drawing.Point(376, 103);
+            this.FlaggedListBox.Margin = new System.Windows.Forms.Padding(2);
+            this.FlaggedListBox.Name = "FlaggedListBox";
+            this.FlaggedListBox.Size = new System.Drawing.Size(131, 238);
+            this.FlaggedListBox.TabIndex = 2;
+            this.FlaggedListBox.SelectedIndexChanged += new System.EventHandler(this.Flagged_SelectedIndexChanged);
+            this.FlaggedListBox.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.FlaggedList_DoubleClick);
+            // 
+            // btn_LoadData
+            // 
+            this.btn_LoadData.Location = new System.Drawing.Point(178, 344);
+            this.btn_LoadData.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_LoadData.Name = "btn_LoadData";
+            this.btn_LoadData.Size = new System.Drawing.Size(46, 21);
+            this.btn_LoadData.TabIndex = 4;
+            this.btn_LoadData.Text = "Load";
+            this.btn_LoadData.UseVisualStyleBackColor = true;
+            this.btn_LoadData.Click += new System.EventHandler(this.btn_LoadData_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(246, 131);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(56, 48);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(246, 235);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(56, 48);
+            this.btnEdit.TabIndex = 6;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(246, 183);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(56, 48);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "-";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // SearchBar
+            // 
+            this.SearchBar.Location = new System.Drawing.Point(46, 32);
+            this.SearchBar.Margin = new System.Windows.Forms.Padding(2);
+            this.SearchBar.Name = "SearchBar";
+            this.SearchBar.Size = new System.Drawing.Size(217, 20);
+            this.SearchBar.TabIndex = 8;
+            this.SearchBar.Text = "Search Bar";
+            // 
+            // btnLinearSearch
+            // 
+            this.btnLinearSearch.Location = new System.Drawing.Point(46, 56);
+            this.btnLinearSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLinearSearch.Name = "btnLinearSearch";
+            this.btnLinearSearch.Size = new System.Drawing.Size(96, 23);
+            this.btnLinearSearch.TabIndex = 9;
+            this.btnLinearSearch.Text = "Linear Search";
+            this.btnLinearSearch.UseVisualStyleBackColor = true;
+            this.btnLinearSearch.Click += new System.EventHandler(this.btnLinearSearch_Click);
+            // 
+            // btnBinarySearch
+            // 
+            this.btnBinarySearch.Location = new System.Drawing.Point(146, 56);
+            this.btnBinarySearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBinarySearch.Name = "btnBinarySearch";
+            this.btnBinarySearch.Size = new System.Drawing.Size(96, 23);
+            this.btnBinarySearch.TabIndex = 10;
+            this.btnBinarySearch.Text = "Binary Search";
+            this.btnBinarySearch.UseVisualStyleBackColor = true;
+            this.btnBinarySearch.Click += new System.EventHandler(this.btnBinarySearch_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(246, 287);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(56, 48);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Clear";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Flagged";
+            // 
+            // TBRegoPlate
+            // 
+            this.TBRegoPlate.Location = new System.Drawing.Point(246, 103);
+            this.TBRegoPlate.Name = "TBRegoPlate";
+            this.TBRegoPlate.Size = new System.Drawing.Size(58, 20);
+            this.TBRegoPlate.TabIndex = 13;
+            this.TBRegoPlate.TextChanged += new System.EventHandler(this.TBRegoPlates_TextChanged);
+            this.TBRegoPlate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Location = new System.Drawing.Point(12, 5);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(49, 22);
+            this.btn_OpenFile.TabIndex = 14;
+            this.btn_OpenFile.Text = "Open File";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.btn_OpenFile_Click_1);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(67, 5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(49, 22);
+            this.btnSave.TabIndex = 15;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnTag
+            // 
+            this.btnTag.Location = new System.Drawing.Point(246, 344);
+            this.btnTag.Margin = new System.Windows.Forms.Padding(2);
+            this.btnTag.Name = "btnTag";
+            this.btnTag.Size = new System.Drawing.Size(53, 29);
+            this.btnTag.TabIndex = 16;
+            this.btnTag.Text = "Tag";
+            this.btnTag.UseVisualStyleBackColor = true;
+            this.btnTag.Click += new System.EventHandler(this.btnTag_Click);
+            // 
+            // RegoPlatesList
+            // 
+            this.RegoPlatesList.FormattingEnabled = true;
+            this.RegoPlatesList.Location = new System.Drawing.Point(48, 110);
+            this.RegoPlatesList.Margin = new System.Windows.Forms.Padding(2);
+            this.RegoPlatesList.Name = "RegoPlatesList";
+            this.RegoPlatesList.Size = new System.Drawing.Size(186, 264);
+            this.RegoPlatesList.TabIndex = 17;
+            this.RegoPlatesList.SelectedIndexChanged += new System.EventHandler(this.RegoPlatesList_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 435);
+            this.Controls.Add(this.btn_LoadData);
+            this.Controls.Add(this.RegoPlatesList);
+            this.Controls.Add(this.btnTag);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Controls.Add(this.TBRegoPlate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnBinarySearch);
+            this.Controls.Add(this.btnLinearSearch);
+            this.Controls.Add(this.SearchBar);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.FlaggedListBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "RegoPlatesDatabase";
+            this.Text = "RegoPlatesDatabase";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox FlaggedListBox;
+        private System.Windows.Forms.SaveFileDialog saveFile;
+        private System.Windows.Forms.Button btn_LoadData;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox SearchBar;
+        private System.Windows.Forms.Button btnLinearSearch;
+        private System.Windows.Forms.Button btnBinarySearch;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBRegoPlate;
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnTag;
+        private System.Windows.Forms.ListBox RegoPlatesList;
+    }
+}
+
---
+++ RegoPlatesDatabase/RegoPlatesDatabase.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RegoPlatesDatabase
+{
+    public partial class RegoPlatesDatabase : Form
+    {
+        public List<string> regoPlate = new List<string>();
+        public List<string> flaggedPlates = new List<string>();
+
+        public RegoPlatesDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private bool checkDuplicate(string plate)
+        {
+            return regoPlate.Contains(plate);
+        }
+
+        private void btn_OpenFile_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Filter = "Text files (day_*.txt)|day_*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+
+                if (File.Exists(filePath))
+                {
+                    string[] lines = File.ReadAllLines(filePath);
+                    if (lines.Length > 0)
+                    {
+                        regoPlate.AddRange(lines);
+                        DisplayLicensePlates(); // Refresh the display
+                        MessageBox.Show("File loaded successfully.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("ERROR: This File is empty. Please choose another file with license plates.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("ERROR: File not found. Please try again with different file.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("ERROR: No Files selected");
+            }
+        }
+
+        private void DisplayLicensePlates()
+        {
+            // sorts licenses alphabetically .Sort()
+            // clear list box
+            // updates the list with sorted rego plates
+            regoPlate.Sort();
+            RegoPlatesList.Items.Clear();
+
+            foreach (string plate in regoPlate)
+            {
+                RegoPlatesList.Items.Add(plate);
+            }
+        }
+
+        private void DisplayFlaggedPlates()
+        {
+            flaggedPlates.Sort();
+            FlaggedListBox.Items.Clear();
+            foreach (string plate in flaggedPlates)
+            {
+                FlaggedListBox.Items.Add(plate);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string newPlate = TBRegoPlate.Text.Trim();
+            
+            if (string.IsNullOrEmpty(newPlate))
+            {
+                MessageBox.Show("ERROR: Input is empty or invalid, please enter a valid license plate");
+                return;
+            }
+            
+            if (checkDuplicate(newPlate)) //todo: create method to check for duplicates
+            {
+                MessageBox.Show("Duplicate license Plate");
+                return;
+            }
+            
+            regoPlate.Add(newPlate);
+            TBRegoPlate.Clear();
+            TBRegoPlate.Focus();
+            DisplayLicensePlates();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (RegoPlatesList.SelectedItem != null)
+            {
+                string originalPlate = RegoPlatesList.SelectedItem.ToString();
+                //show in textbox to edit
+                string newPlate = TBRegoPlate.Text.Trim();
+
+                if (string.IsNullOrEmpty(newPlate))
+                {
+                    MessageBox.Show("ERROR: Input is empty or invalid, please enter a valid license plate");
+                    return;
+                }
+                if (checkDuplicate(newPlate))
+                {
+                    MessageBox.Show("ERROR: Duplicate License Plate");
+                    return;
+                }
+
+                int selectedIndex = RegoPlatesList.SelectedIndex;
+                regoPlate[selectedIndex] = newPlate;  // Update the plate in the list
+                DisplayLicensePlates();  // Reloads the list
+                TBRegoPlate.Clear();
+                TBRegoPlate.Focus();
+                MessageBox.Show("License Plate successfully edited");
+            }
+            else
+            {
+                MessageBox.Show("ERROR: Please select a License Plate to edit");
+            }
+        }
+
+        private void RegoPlatesList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (RegoPlatesList.SelectedItems != null)
+            {
+                string selectedPlate = RegoPlatesList.SelectedItem.ToString();
+                TBRegoPlate.Text = selectedPlate;
+            }
+            else
+            {
+                TBRegoPlate.Clear();
+                TBRegoPlate.Focus();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (RegoPlatesList.SelectedItem != null)
+            {
+                string selectedPlate = RegoPlatesList.SelectedItem.ToString();
+                regoPlate.Remove(selectedPlate);
+                DisplayLicensePlates();  // Refresh the display
+                SearchBar.Clear();
+                SearchBar.Focus();
+            }
+            else
+            {
+                MessageBox.Show("ERROR: Please select a License Plate to remove.");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (day_*.txt)|day_*.txt";
+            saveFileDialog.Title = "Save License Plates";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(saveFileDialog.FileName, regoPlate);
+                MessageBox.Show("Data saved successfully.");
+            }
+        }
+
+        private void autoIncrementing()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            string[] existingFiles = Directory.GetFiles(directory, "day_*.txt");
+
+            int fileIndex = existingFiles.Length + 1;
+            string fileName = $"day.{fileIndex}";
+        }
+
+        private void btnBinarySearch_Click(object sender, EventArgs e)
+        {
+            string target = SearchBar.Text.Trim();
+
+            if (string.IsNullOrEmpty(target))
+            {
+                MessageBox.Show("ERROR: Please enter a valid license plate to search.");
+                return;
+            }
+
+            regoPlate.Sort();
+            int index = regoPlate.BinarySearch(target);
+
+            if (index >= 0)
+            {
+                RegoPlatesList.SelectedItem = regoPlate[index];
+                MessageBox.Show($"License Plate {target} found.");
+            }
+            else
+            {
+                MessageBox.Show($"License Plate {target} not found.");
+                SearchBar.Clear();
+                SearchBar.Focus();
+            }
+        }
+
+        private void btnLinearSearch_Click(object sender, EventArgs e)//todo: check if it follow guidelines/meets requirements 
+        {
+            string target = SearchBar.Text.Trim();
+
+            if (string.IsNullOrEmpty(target))
+            {
+                MessageBox.Show("ERROR: Please enter a valid license plate to search.");
+                return;
+            }
+
+            int index = regoPlate.IndexOf(target);
+            if (index >= 0)
+            {
+                RegoPlatesList.SelectedItem = regoPlate[index];
+                MessageBox.Show($"License Plate {target} found.");
+            }
+            else
+            {
+                MessageBox.Show($"License Plate {target} not found.");
+                SearchBar.Clear();
+                SearchBar.Focus();
+            }
+        }
+
+        private void btnTag_Click(object sender, EventArgs e)
+        {
+            if (RegoPlatesList.SelectedItem != null)
+            {
+                string tagplate = RegoPlatesList.SelectedItem.ToString();
+                regoPlate.Remove(tagplate);
+                flaggedPlates.Add(tagplate);
+                DisplayLicensePlates();
+                DisplayFlaggedPlates();
+            }
+            else
+            {
+                MessageBox.Show("ERROR: Select a license to tag");
+            }
+        }
+
+        private void FlaggedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (FlaggedListBox.SelectedItems != null)
+            {
+                string selectedPlate = FlaggedListBox.SelectedItems.ToString();
+                TBRegoPlate.Text = selectedPlate;
+            }
+            else
+            {
+                TBRegoPlate.Clear();
+                TBRegoPlate.Focus();
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // list<>
+        }
+
+        private void Flagged_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // list<> flagged
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e) //todo: or keyboard Enter?
+        {
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void FlaggedList_DoubleClick(object sender, MouseEventArgs e)
+        {
+            if (FlaggedListBox.SelectedItem != null)
+            {
+                string unflag = FlaggedListBox.SelectedItem.ToString();
+                flaggedPlates.Remove(unflag);
+                regoPlate.Add(unflag);
+                DisplayLicensePlates();
+                DisplayFlaggedPlates();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            regoPlate.Clear();
+            flaggedPlates.Clear();
+            DisplayLicensePlates();
+            DisplayFlaggedPlates();
+            SearchBar.Clear();
+            SearchBar.Focus();
+        }
+
+        private void TBRegoPlates_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_LoadData_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
